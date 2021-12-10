967d8dd917cc81438beab5b3b08be9fc99b83859
Gururaj S <gururaj.sudhindra@gmail.com>
2021-12-10 20:41:02 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://19a202f9.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://718005b8.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
